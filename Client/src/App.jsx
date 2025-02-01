63b02cd2638fcccca40623cbe71eadd5eea3ddb8
Rutu <rutubhimani124@gmail.com>
2025-02-01 17:20:46 +0530

    Add appointment management components and update ESLint configuration

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,27 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import PatientSignUp from './pages/PatientSignUp'
-import DoctorSignUp from './pages/DoctorSignUp'
-import SignUp from './pages/Signup'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PatientSignUp from "./pages/PatientSignUp";
+import DoctorSignUp from "./pages/DoctorSignUp";
+import SignUp from "./pages/Signup";
+import Appointment from "./pages/Appointment";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}>
+          <Route path="/appointment" element={<Appointment />} />
             {/* <Route path="/" /> */}
           </Route>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/signup/patient' element={<PatientSignUp />} />
-          <Route path='/signup/doctor' element={<DoctorSignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signup/patient" element={<PatientSignUp />} />
+          <Route path="/signup/doctor" element={<DoctorSignUp />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
