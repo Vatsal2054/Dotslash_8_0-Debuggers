1bb0149e9c605ada4895f7a82405a746bf239a82
jaldip12 <jaldipvekariya@gmail.com>
2025-02-01 16:10:50 +0530

    `/getDoctor endpoint instead of /`

--- Backend/src/routes/doctor.routes.js
+++ Backend/src/routes/doctor.routes.js
@@ -3,6 +3,6 @@ import { getDoctor } from '../controller/doctor.controller.js';
 
 const router = Router();
 
-router.get('/', getDoctor);
+router.get('/getDoctor', getDoctor);
 
 export default router;
