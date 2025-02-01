a0dc1b8660f09c49b8c2ca477b6e7fe4e5ee14ad
Rutu <rutubhimani124@gmail.com>
2025-02-01 12:04:41 +0530

    user model added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,27 +1,46 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import { rateLimit } from "express-rate-limit";
+
+dotenv.config();
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+});
+
 const app = express();
 
-const logger = morgan("dev");
-
-app.use(logger);
-
 app.use(
-    cors({
-      origin: "*", // Allow access from everywhere
-      credentials: true,
-    })
-  );
-  app.use(express.static("public"));
-  app.use(cookieParser());
-  
-  app.use(express.json());
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "UPDATE", "PUT"],
+  })
+);
+app.use(limiter);
 
-app.get("/", (req, res) => {
-    res.send("Hello from the server!");
-  });
+app.use(express.json({ limit: "1mb" }));
+app.use(cookieParser());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
+// Routes
 
-  export { app };
+// import authRoutes from "./routes/auth.routes.js";
+// import taskRoutes from "./routes/task.routes.js";
+// import dashboardRoutes from "./routes/dashboard.routes.js";
+
+// app.use("/auth", authRoutes);
+
+// app.use("/task", taskRoutes);
+
+// app.use("/dashboard", dashboardRoutes);
+
+export default app;
