3fafdafd3e3618bdcb7e65e289d63856f04602ac
Rutu <rutubhimani124@gmail.com>
2025-02-02 12:13:54 +0530

    commit

--- Client/src/components/Homepage/DoctorHome.jsx
+++ Client/src/components/Homepage/DoctorHome.jsx
@@ -167,8 +167,8 @@ export default function DoctorHome() {
                 </div>
                 <div
                   className={`px-4 py-2 rounded-full text-sm font-medium ${
-                    apt.status === "Confirmed"
-                      ? "bg-green-100 text-green-800 dark:bg-green-800 dark:text-green-100"
+                    apt.status === "approved"
+                      ? "bg-green text-emerald-100 dark:bg-green-800 dark:text-green-100"
                       : "bg-yellow-100 text-yellow-800 dark:bg-yellow-800 dark:text-yellow-100"
                   }`}
                 >
